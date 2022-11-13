9877a779d368d74d99248c541de4a92990e59223
Bruno Martins <brunomart97@hotmail.com>
2022-11-13 16:17:01 -0300

    Pool creation route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import shortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
   log: ['query']
@@ -19,6 +21,26 @@ const bootstrap = async () => {
     const count = await prisma.pool.count()
 
     return { count }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBy = z.object({
+      title: z.string()
+    })
+
+    const { title } = createPoolBy.parse(request.body)
+
+    const generate = new shortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code: code
+      }
+    })
+
+    return reply.status(201).send({ code })
   })
 
   await fastify.listen({ port: 3333 })
